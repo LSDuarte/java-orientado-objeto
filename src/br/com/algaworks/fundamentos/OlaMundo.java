1c7e7959c799f5de8302d1622c4a330fc8ffb0be
Larissa Duarte <42499@pjmt.local>
2021-02-13 12:49:51 -0400

    iniciando fundamentos java

---
+++ src/br/com/algaworks/fundamentos/OlaMundo.java
@@ -0,0 +1,7 @@
+package br.com.algaworks.fundamentos;
+
+public class OlaMundo {
+	public static void main(String[] args) {
+		System.out.println("Olá, mundo!");
+	}
+}
---
+++ src/br/com/algaworks/fundamentos/exercicios/OlaGalera.java
@@ -0,0 +1,7 @@
+package br.com.algaworks.fundamentos.exercicios;
+
+public class OlaGalera {
+	public static void main(String[] args) {
+		System.out.println("Olá, galera!");
+	}
+}
